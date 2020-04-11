e9d493a868c1b6b6310b01ff10d97dc8ab8ae0eb
moesharshar <33705287+moesharshar@users.noreply.github.com>
2020-04-11 18:57:09 +0200

    Update cnn.py

--- 7th_Lab(Online)_CNN/cnn.py
+++ 7th_Lab(Online)_CNN/cnn.py
@@ -37,10 +37,10 @@ from keras import models
 train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
-training_set = train_datagen.flow_from_directory('dataset/training_set', target_size=(64, 64), batch_size=32,  # 32 not 10
+training_set = train_datagen.flow_from_directory('dataset/training_set', target_size=(64, 64), batch_size=32,
                                                  class_mode='binary')
 
-testing_set = test_datagen.flow_from_directory('dataset/test_set', target_size=(64, 64), batch_size=32,  # 32 not 10
+testing_set = test_datagen.flow_from_directory('dataset/test_set', target_size=(64, 64), batch_size=32,
                                                class_mode='binary')
 
 classifier.fit_generator(training_set, samples_per_epoch=8000, nb_epoch=25, validation_data=testing_set,
